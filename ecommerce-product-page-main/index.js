5c258dd4b9dfa9abb47e221a2bfc9432ea3edc78
ranyo7 <65369571+ranyo7@users.noreply.github.com>
2022-02-10 15:02:40 -0800

    added overlay functionality

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -1,13 +1,22 @@
 var orderAmount = 1;
+var overSelector = 1;
 
 $(document).ready(function() {
 
   $("#cartImage").click(function(){
-    $("#cart").addClass("dropDown");
+    $("#cart").addClass("visible");
   });
 
   $("#cart").mouseleave(function(){
-    $("#cart").removeClass("dropDown");
+    $("#cart").removeClass("visible");
+  });
+
+  $("#closeOverlay").click(function(){
+    $("#overlay").hide();
+  });
+
+  $("#displayImage").click(function(){
+    $("#overlay").show();
   });
 
   $(".thumbnail").click(function(){
@@ -17,17 +26,55 @@ $(document).ready(function() {
     $("#displayImage").attr("src", image);
   });
 
-  updateOrderAmount();
+  $(".oThumbnail").click(function(){
+    $(".oThumbnail").removeClass("thumbnailSelected");
+    $(this).addClass("thumbnailSelected");
+    var image = $(this).attr("src").replace("-thumbnail","");
+    $("#overlayImg").attr("src", image);
+  });
+
+  $(".trashcan").click(function(){
+    $(".cartItem").addClass("invisible").removeClass("gridCol");
+    $(".checkout").addClass("invisible");
+    $(".centerGray").removeClass("invisible");
+  });
+
+  $(".addToCart").click(function(){
+    //Make cart visible (feedback to user)
+    $("#cart").addClass("visible");
+    //Set the "You have nothing in your cart" to not visible
+    $(".centerGray").addClass("invisible");
+    //Turn cart item and checkoutButton to be visible
+    $(".cartItem").removeClass("invisible").addClass("gridCol");
+    $(".checkout").removeClass("invisible");
+    //Update cart with the chosen amount
+    $("#cartAmount").text("$125.00 x "+ orderAmount);
+    $("#cartTotal").text("$"+(orderAmount*125)+".00");
+  });
 
   $("#incOrderAmount").click(function(){
     orderAmount+=1;
     updateOrderAmount();
   });
+
   $("#decOrderAmount").click(function(){
     orderAmount-=1;
     updateOrderAmount();
   });
+
+  updateOrderAmount();
+
+
 });
+
+function toggleOverlay(){
+  var type = $("#overlay").style.display;
+  if(type == "block") {
+    type = "none";
+  } else {
+    type = "block";
+  }
+}
 
 function updateOrderAmount(){
   orderAmount = Math.min(Math.max(orderAmount, 1), 10);
