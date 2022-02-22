b7e89349177857dffe0bec9d2d46eae009639029
ranyo7 <65369571+ranyo7@users.noreply.github.com>
2022-02-22 13:40:13 -0800

    finished up main page design and backend code

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -1,13 +1,15 @@
-getAJAX();
+getAJAX('all');
+var darkmode = false;
 
-function getAJAX(){
+function getAJAX(input){
   console.log("Loaded getAJAX()");
+  console.log(input);
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
     const response = this.responseText;
     createTable(response);
     }
-  xhttp.open("GET", `https://restcountries.com/v3.1/all`);
+  xhttp.open("GET", `https://restcountries.com/v3.1/${input}`);
   xhttp.send();
 }
 
@@ -19,8 +21,8 @@ function createTable(data){
 
     for (let i=0; i<dataSet.length; i++){
       table +=
-      "<div class='card'>" +
-      `<img src="${dataSet[i].flags.png}" class="card-img-top" alt="...">` +
+      "<div class='card mx-auto DML'>" +
+        `<img src="${dataSet[i].flags.png}" class="card-img-top" alt="...">` +
         "<div class='card-body'>" +
           "<div class=''>" +
             `<h5 class=''>${dataSet[i].name.common}</h6>` +
@@ -31,13 +33,23 @@ function createTable(data){
         "</div>" +
       "</div>";
     }
-    document.getElementById("display").innerHTML = table;
+    $("#display").html(table);
+
+    if(darkmode){
+      $("#display .DML").addClass("dark-mode-light");
+    }
   } else {
     document.getElementById("display").innerHTML = "Data set is null";
   }
 }
 
 function darkModeToggle(){
+  darkmode = !darkmode;
   $("body").toggleClass("dark-mode");
   $(".DML").toggleClass("dark-mode-light");
+}
+
+function searchByName(){
+  var input = $("#query").val();
+  getAJAX('name/'+input);
 }
