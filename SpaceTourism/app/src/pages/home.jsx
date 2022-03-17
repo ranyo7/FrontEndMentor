cc6c4f268956e010a0c3ca7eaad7f131dbdb5f0c
ranyo7 <65369571+ranyo7@users.noreply.github.com>
2022-03-17 14:19:12 -0700

    finished react portion of the site

--- SpaceTourism/app/src/pages/home.jsx
+++ SpaceTourism/app/src/pages/home.jsx
@@ -1,30 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Header from "./Components/header.jsx";
 
 const Home = () => {
   return (
-    <div className="home">
-      <a className="skip-to-content" href="#main">Skip to main content</a>
-      <header className="primary-header flex">
+    <div className="home background">
 
-        <div>
-          <img src="./assets/shared/logo.svg" alt="space tourism log" className="logo"></img>
-        </div>
-
-        <button className="mobile-nav-toggle" aria-controls="primary-navigation" >
-          <span className="sr-only" aria-expanded="false"> Menu </span>
-        </button>
-
-        <nav>
-          <ul id="primary-navigation" data-visible="false" className="primary-navigation underline-indicators flex">
-            <li className="active"><Link to="/home"><span aria-hidden="true">00</span>Home</Link></li>
-            <li><Link to="/destination"><span aria-hidden="true">01</span>Destination</Link></li>
-            <li><Link to="/crew"><span aria-hidden="true">02</span>Crew</Link></li>
-            <li><Link to="/technology"><span aria-hidden="true">03</span>Technology</Link></li>
-          </ul>
-        </nav>
-
-      </header>
+      <Header />
 
       <main id="main" className="grid-container">
         <div>
@@ -36,7 +18,7 @@ const Home = () => {
         </div>
 
         <div>
-          <a href="./destination.html" className="large-button uppercase">Explore</a>
+          <Link to="/destination" className="large-button uppercase">Explore</Link>
         </div>
       </main>
 
--- SpaceTourism/app/src/pages/technology.jsx
+++ SpaceTourism/app/src/pages/technology.jsx
@@ -1,62 +1,95 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import Header from "./Components/header.jsx";
+import DATA from "./data";
 
 const Technology = () => {
   return (
-    <div className="technology">
-      <a className="skip-to-content" href="#main">Skip to main content</a>
-      <header className="primary-header flex">
+    <div className="technology background">
 
-        <div>
-          <img src="./assets/shared/logo.svg" alt="space tourism log" className="logo"></img>
-        </div>
-
-        <button className="mobile-nav-toggle" aria-controls="primary-navigation" >
-          <span className="sr-only" aria-expanded="false"> Menu </span>
-        </button>
-
-        <nav>
-          <ul id="primary-navigation" data-visible="false" className="primary-navigation underline-indicators flex">
-            <li><Link to="/home"><span aria-hidden="true">00</span>Home</Link></li>
-            <li><Link to="/destination"><span aria-hidden="true">01</span>Destination</Link></li>
-            <li><Link to="/crew"><span aria-hidden="true">02</span>Crew</Link></li>
-            <li className="active"><Link to="/technology"><span aria-hidden="true">03</span>Technology</Link></li>
-          </ul>
-        </nav>
-
-      </header>
+      <Header />
 
       <main id="main" className="grid-container flow">
 
         <h1 className="numbered-title"><span aria-hidden="true">03</span>Space Launch 101</h1>
 
-        <picture>
-          <source media="(min-width: 45em)" srcset="./assets/technology/image-launch-vehicle-portrait.jpg" type="image/jpg" />
-          <img className="image" src="./assets/technology/image-launch-vehicle-landscape.jpg" alt="Rocket Launch" />
-        </picture>
+        <Image />
 
         <div className="number-indicators flex">
-          <button id="launch" aria-selected="true">1</button>
-          <button id="spaceport" aria-selected="false">2</button>
-          <button id="capsule" aria-selected="false">3</button>
+          <button id="launch" aria-selected="true" onClick={() => handleTabClick("launch")}>1</button>
+          <button id="spaceport" aria-selected="false" onClick={() => handleTabClick("spaceport")}>2</button>
+          <button id="capsule" aria-selected="false" onClick={() => handleTabClick("capsule")}>3</button>
         </div>
 
         <article className="technology-info">
           <header className="flow flow--sm">
             <h2 className="fs-400 uppercase ff-serif text-light">The terminology...</h2>
-            <p className="fs-700 ff-serif uppercase name">Launch Vehicle</p>
+            <Terminology />
           </header>
 
-          <p className="description">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a
-          payload from Earth's surface to space, usually to Earth orbit or beyond. Our
-          WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall,
-          it's quite an awe-inspiring sight on the launch pad!</p>
+          <Description />
         </article>
 
       </main>
 
     </div>
   );
+
+  function handleTabClick(input){
+    //deactivate tabs
+    document.querySelectorAll(".number-indicators > button").forEach( function(button) {
+      button.ariaSelected = false;
+    });
+    //activate tabs
+    document.querySelector("#"+input).ariaSelected = true;
+    //Update crew information and images
+    switch(input){
+      case "launch": updateInformation(DATA.technology[0]);
+        break;
+      case "spaceport": updateInformation(DATA.technology[1]);
+        break;
+      case "capsule": updateInformation(DATA.technology[2]);
+        break;
+    }
+
+  }
+
+  function updateInformation(input){
+    //Update images
+    const image = [...document.querySelector("picture").children];
+    image[0].setAttribute("srcSet", input.images.portrait);
+    image[1].setAttribute("src", input.images.landscape);
+    image[1].setAttribute("alt", input.name);
+    //Update Title
+    document.querySelector(".name").textContent = input.name;
+    //Update description
+    document.querySelector(".description").textContent = input.description;
+  }
 };
 
 export default Technology;
+
+function Image(){
+  return (
+    <picture>
+      <source media="(min-width: 45em)" srcSet="./assets/technology/image-launch-vehicle-portrait.jpg" type="image/jpg" />
+      <img className="image" src="./assets/technology/image-launch-vehicle-landscape.jpg" alt="Rocket Launch" />
+    </picture>
+  );
+}
+
+function Terminology(){
+  return (
+    <p className="fs-700 ff-serif uppercase name">Launch Vehicle</p>
+  );
+}
+
+function Description(){
+  return (
+    <p className="description">
+      A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a
+      payload from Earth's surface to space, usually to Earth orbit or beyond. Our
+      WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall,
+      it's quite an awe-inspiring sight on the launch pad!
+    </p>
+  );
+}
